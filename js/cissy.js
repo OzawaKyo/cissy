10b2492728cab87df6e2bfcd7b0f50c54b96cc20
Mohamed Amarcha <simoamarcha@icloud.com>
2023-05-29 21:21:24 +0100

    gh

--- js/cissy.js
+++ js/cissy.js
@@ -1,8 +1,16 @@
 const menuIcon = document.querySelector('.menu-icon');
 const menuText = document.querySelector('.menu');
+const categorie = document.querySelector('.categories')
+const star = document.querySelector('.star')
+const border = document.querySelector('.navv')
+
 
 menuIcon.addEventListener('click', function() {
     this.classList.toggle('active');
     menuText.textContent = this.classList.contains('active') ? 'close' : 'menu';
     menuText.classList.toggle('active');
+    categorie.classList.toggle('active');
+    star.classList.toggle('active');
+    border.classList.toggle('active');
+
 });
